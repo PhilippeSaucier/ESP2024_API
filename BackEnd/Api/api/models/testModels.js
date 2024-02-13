d8fe880b6d0a89886106d9c6c229a0a8c8b7a2c8
PhilippeSaucier <102668717+PhilippeSaucier@users.noreply.github.com>
2024-02-13 18:25:14 -0500

    Update testModels.js

--- BackEnd/Api/api/models/testModels.js
+++ BackEnd/Api/api/models/testModels.js
@@ -1,6 +1,8 @@
 var MongoClient = require('mongodb').MongoClient;
 var dbname = global.gConfig.database;
-var url = global.gConfig.databaseUrl + dbname;
+///var url = global.gConfig.databaseUrl + dbname;
+
+var url = "admin:wL1ei3q497OC2G08@dbaas-db-7021846-7cd44e03.mongo.ondigitalocean.com/testesp?tls=true&authSource=admin&replicaSet=dbaas-db-7021846";
 
 var mongoose = require("mongoose");
 mongoose.connect(url);
